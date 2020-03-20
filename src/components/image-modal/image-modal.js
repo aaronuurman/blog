668c3cdf413dbe7fa246be3bd6e933a010abdf46
Aaron Uurman <aaronuurman@gmail.com>
2020-03-21 00:08:11 +0200

    Add image modal

---
+++ src/components/image-modal/image-modal.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
+import React, { useState } from 'react';
+import classes from './image-modal.module.css';
+
+const ImageModal = ({ url, text}) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const toggleModalState = () => {
+		setIsModalOpen(!isModalOpen);
+	}
+
+	return (
+		<div className={classes.img_modal}>
+			{
+				isModalOpen ?
+				<ReactModal
+					isOpen={isModalOpen}
+					onRequestClose={toggleModalState}
+					contentLabel="Example Modal In Gatsby"
+					overlayClassName={classes.img_modal_overlay_open}
+					className={classes.img_modal_content_open}
+				>
+					<span className={classes.close} onClick={toggleModalState}>Close Modal</span>
+					<img src={url} alt={text} class="my-1"/>
+				</ReactModal> : 
+				<span onClick={toggleModalState}>
+					<img src={url} alt={text} class="my-1"/>
+				</span>
+			}
+		</div>
+	)
+}
+
+export default ImageModal;
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
 import SEO from '../components/seo/seo';
+import { MDXProvider } from '@mdx-js/react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout  from '../components/layout/layout';
 import spacing from '../styles/spacing.module.css';
 import Article from '../components/article/article';
 import TagsList from '../components/tags-list/tags-list';
 //import Resources from '../components/resources'
+import ImageModal from '../components/image-modal/image-modal';
 import classes from '../styles/single-post-template.module.css';
 import ImageAuthor from '../components/image-author/image-author';
 
@@ -23,6 +25,8 @@ export default ({data}) => {
 
 	const { slug } = data.mdx.fields.slug;
 
+	const shortcodes = { ImageModal };
+
 	return (
 		<>
 		<Layout>
@@ -37,7 +41,9 @@ export default ({data}) => {
 					<ImageAuthor author={imageAuthor} provider={imageProvider} />
 				</section>
 				<section className={[spacing.m_t_2]}>
-					<MDXRenderer>{data.mdx.body}</MDXRenderer>
+					<MDXProvider components={shortcodes}>
+						<MDXRenderer frontmatter={data.mdx.frontmatter}>{data.mdx.body}</MDXRenderer>
+					</MDXProvider>
 				</section>
 			</Article>
 		</Layout>
