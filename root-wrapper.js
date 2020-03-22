4b712fcceb3b1ef7450942a900cf894dd5be90d4
Aaron Uurman <aaronuurman@gmail.com>
2020-03-22 13:52:29 +0200

    Add code block style

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,46 +1,24 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import theme from 'prism-react-renderer/themes/nightOwl';
-import Highlight, { defaultProps } from 'prism-react-renderer';
+import Code from './src/components/code/code';
 
 const components = {
 	'p.inlineCode': props => (
 		<code style={{ backgroundColor: 'lightgray' }} {...props} />
 	),
-	pre: props => {
-		const className = props.children.props.className || '';
-		const matches = className.match(/language-(?<lang>.*)/);
-		
-		return (
-			<Highlight
-				{...defaultProps}
-				code={props.children.props.children.trim()}
-				language={
-					matches && matches.groups && matches.groups.lang
-						? matches.groups.lang
-						: ''
-				}
-				theme={theme}
-			>
-				{({
-					className,
-					style,
-					tokens,
-					getLineProps,
-					getTokenProps,
-				}) => (
-					<pre className={className} style={style}>
-						{tokens.map((line, i) => (
-							<div {...getLineProps({ line, key: i })}>
-								{line.map((token, key) => (
-									<span {...getTokenProps({ token, key })} />
-								))}
-							</div>
-						))}
-					</pre>
-				)}
-			</Highlight>
-		);
+	pre: ({children: {props}}) => {		
+		if (props.mdxType === 'code') {
+			return (
+				<Code 
+					codeString={props.children.trim()}
+					language={
+						props.className && 
+						props.className.replace('language-', '')
+					}
+					{...props}
+				/>
+			);
+		}
 	},
 };
 
---
+++ src/components/code/code.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classes from './code.module.css';
+import spacing from '../../styles/spacing.module.css';
+import theme from 'prism-react-renderer/themes/nightOwl';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+
+const Code = ({ codeString, language }) => {
+  return (
+    <Highlight
+      {...defaultProps}
+      code={codeString}
+      language={language}
+      theme={theme}
+    >
+      {({
+        className,
+        style,
+        tokens,
+        getLineProps,
+        getTokenProps,
+      }) => (
+        <pre className={[className, classes.code, spacing.m_y_1, spacing.p_1].join(' ')} style={style}>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })}>
+							<span className={classes.line_nr}>{i + 1}</span>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+export default Code;
