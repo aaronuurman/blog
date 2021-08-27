7bbfeefeb9410118a4a176d55942fb18eadfcdac
Aaron Uurman <aaronuurman@gmail.com>
2021-08-27 12:38:11 +0300

    feat: add nav mock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>About</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <Component {...pageProps} />
+        </div>
+    )
 }
 export default MyApp
---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+                <meta name="description" content="About" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1> About </h1>
+            </main>
+        </div>
+    )
+}
+
+export default About
