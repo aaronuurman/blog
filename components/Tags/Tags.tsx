1cc73ae9116d41eafbef26a850ee07e41a0b6f85
Aaron Uurman <aaronuurman@gmail.com>
2021-08-29 20:17:12 +0300

    feat: fix tags colors

--- components/Tags/Tags.tsx
+++ components/Tags/Tags.tsx
@@ -18,7 +18,7 @@ const Tags = ({ tags }: Props) => {
         <div className={[classes.tags]}>
             {tags.sort().map((tag, index) => (
                 <Link key={index} href={`/tag/${slugify(tag)}`}>
-                    <div className={[classes[camelize(tag)], spacing.p_l_2, spacing.p_r_08, spacing.p_y_04].join(' ')}>
+                    <div className={[classes[slugify(tag)], spacing.p_l_2, spacing.p_r_08, spacing.p_y_04].join(' ')}>
                         {tag}
                     </div>
                 </Link>
